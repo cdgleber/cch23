5ebe27e857f201964f37787f899d2575f302b59f
Conrad Gleber <cdgleber@gmail.com>
2024-12-28 07:41:17 -0500

    d11 complete

--- src/days/day11.rs
+++ src/days/day11.rs
@@ -1,22 +1,28 @@
-use std::io::Cursor;
-
 use axum::{extract::Multipart, response::IntoResponse, routing::post, Router};
 use image::{GenericImageView, ImageReader};
+use std::io::Cursor;
 use tower_http::services::ServeDir;
+
+fn stringify<T: std::fmt::Debug>(x: T) -> String {
+    format!("error code: {:?}", x)
+}
 
 async fn magic_reds(mut multipart: Multipart) -> impl IntoResponse {
     let mut magic_count = 0u32;
     if let Some(field) = multipart.next_field().await.unwrap() {
         let _name = field.name().unwrap().to_string();
         let data = field.bytes().await.unwrap();
-
-        let reader = ImageReader::new(Cursor::new(data))
-            .with_guessed_format()
-            .expect("Cursor io never fails");
-        let image = reader.decode().unwrap(); //ERROR HERE
+        let mut reader = ImageReader::new(Cursor::new(data));
+        reader.set_format(image::ImageFormat::Png);
+        let image = match reader.decode() {
+            Ok(i) => i,
+            Err(e) => {
+                return stringify(e);
+            }
+        };
 
         for (_, _, rgb) in image.pixels() {
-            if rgb[0] > rgb[1] + rgb[2] {
+            if rgb[0] > rgb[1].saturating_add(rgb[2]) {
                 magic_count += 1;
             }
         }
