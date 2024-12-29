59a22b50fdc68491d1572086b93eb7e261bb4817
cgleber <conrad_gleber@urmc.rochester.edu>
2024-12-29 12:02:56 -0500

    continue d15 finished joy

--- src/days/day15.rs
+++ src/days/day15.rs
@@ -1,18 +1,12 @@
-use std::collections::{HashMap, HashSet};
+use std::collections::{ HashMap, HashSet };
 
-use axum::{
-    http::StatusCode,
-    response::{IntoResponse, Response},
-    routing::post,
-    Json, Router,
-};
-use serde::{Deserialize, Serialize};
+use axum::{ http::StatusCode, response::{ IntoResponse, Response }, routing::post, Router };
+use serde::{ Deserialize, Serialize };
 use thiserror::Error;
 
 #[derive(Error, Debug)]
 enum AppError {
-    #[error("Parse error")]
-    ParseError(serde_json::Error),
+    #[error("Parse error")] ParseError(serde_json::Error),
 }
 
 impl IntoResponse for AppError {
@@ -48,7 +42,10 @@ impl NiceInput {
         }
 
         let vowels: HashSet<char> = "aeiouy".chars().collect();
-        let num_vowels = self.input.chars().filter(|c| vowels.contains(c)).count();
+        let num_vowels = self.input
+            .chars()
+            .filter(|c| vowels.contains(c))
+            .count();
         if num_vowels < 3 {
             return "naughty".to_string();
         }
@@ -60,7 +57,9 @@ impl NiceInput {
 async fn nice(body: String) -> Result<impl IntoResponse, AppError> {
     let input: NiceInput = match serde_json::from_str(&body) {
         Ok(input) => input,
-        Err(e) => return Err(AppError::ParseError(e)),
+        Err(e) => {
+            return Err(AppError::ParseError(e));
+        }
     };
 
     let temp_map = HashMap::from([("result".to_string(), input.is_nice())]);
@@ -70,14 +69,59 @@ async fn nice(body: String) -> Result<impl IntoResponse, AppError> {
 async fn game(body: String) -> Result<impl IntoResponse, AppError> {
     let input: NiceInput = match serde_json::from_str(&body) {
         Ok(input) => input,
-        Err(e) => return Err(AppError::ParseError(e)),
+        Err(e) => {
+            return Err(AppError::ParseError(e));
+        }
     };
 
-    Ok((StatusCode::OK, input.input))
+    if input.input.len() < 8 {
+        let ret_string = format!(r#"{{"result":"naughty","reason":"8 chars"}}"#);
+        return Ok((StatusCode::BAD_REQUEST, ret_string));
+    }
+
+    if
+        !input.input.chars().any(|c| c.is_uppercase()) ||
+        !input.input.chars().any(|c| c.is_lowercase()) ||
+        !input.input.chars().any(|c| c.is_digit(10))
+    {
+        let ret_string = format!(r#"{{"result":"naughty","reason":"more types of chars"}}"#);
+        return Ok((StatusCode::BAD_REQUEST, ret_string));
+    }
+
+    if
+        input.input
+            .chars()
+            .filter(|c| c.is_digit(10))
+            .count() < 5
+    {
+        let ret_string = format!(r#"{{"result":"naughty","reason":"55555"}}"#);
+        return Ok((StatusCode::BAD_REQUEST, ret_string));
+    }
+
+    let sum_digits: u32 = input.input
+        .split(|c: char| !c.is_numeric())
+        .filter_map(|x| x.parse::<u32>().ok())
+        .sum::<u32>();
+
+    if sum_digits != 2023u32 {
+        let ret_string = format!(r#"{{"result":"naughty","reason":"math is hard"}}"#);
+        return Ok((StatusCode::BAD_REQUEST, ret_string));
+    }
+
+    let joy_vec: Vec<char> = input.input
+        .chars()
+        .filter(|c| (*c == 'j' || *c == 'o' || *c == 'y'))
+        .collect();
+    let joy_dir = joy_vec.windows(3).all(|s| s[0] == 'j' && s[1] == 'o' && s[2] == 'y');
+    if !joy_dir {
+        let ret_string = format!(r#"{{"result":"naughty","reason":"not joyful enough"}}"#);
+        return Ok((StatusCode::NOT_ACCEPTABLE, ret_string));
+    }
+
+    let ret_string = format!(r#"{{"result":"nice","reason":"that's a nice password"}}"#);
+    return Ok((StatusCode::OK, ret_string));
 }
 
 pub fn router() -> Router {
-    Router::new()
-        .route("/nice", post(nice))
-        .route("/game", post(game))
+    Router::new().route("/nice", post(nice)).route("/game", post(game))
 }
